4d4a00250ec642440aba9bb7668f18600892bfc8
hello <ngoxleon3@gmail.com>
2024-02-03 11:19:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+class Cat:
+    def __init__(self, name):
+        self.name = Cat
+        self.gladness = 50
+        self.calmness = 50
+        self.hunger = 30
+        self.home = True
+
+    def to_eat(self):
+            pass
+
+    def drink(self):
+            pass
+
+    def chill(self):
+            pass
+
+    def is_alive(self):
+            pass
+        
+    def live(self, day):
+            pass
+
+
+
+
